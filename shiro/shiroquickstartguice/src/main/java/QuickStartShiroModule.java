5e78a0061aebee8162f03e286c9be22114eb5e0b
SPC1996 <1594475695@qq.com>
2017-10-07 16:20:29 +0800

    add shiro-quickstart-guice

---
+++ shiro/shiroquickstartguice/src/main/java/QuickStartShiroModule.java
@@ -0,0 +1,20 @@
+import com.google.inject.Provides;
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.guice.ShiroModule;
+import org.apache.shiro.realm.text.IniRealm;
+
+public class QuickStartShiroModule extends ShiroModule {
+    @Override
+    protected void configureShiro() {
+        try {
+            bindRealm().toConstructor(IniRealm.class.getConstructor(Ini.class));
+        } catch (NoSuchMethodException e) {
+            addError(e);
+        }
+    }
+
+    @Provides
+    Ini loadShiroIni() {
+        return Ini.fromResourcePath("classpath:shiro.ini");
+    }
+}
